e542e1b46c0c68f0d10c87a2f084df300006f4cc
ryutomukai <ryuto.mukai1994@gmail.com>
2022-04-19 14:51:31 +0000

    通知機能ビューページ

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -3,7 +3,7 @@ class Public::NotificationsController < ApplicationController
   def index
     @notifications = current_user.passive_notifications.page(params[:page]).per(20)
     @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
+      notification.update(checked: true)
     end
   end
 end
