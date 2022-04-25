9f4805903fb7e0870220033f27c5c92106e5c2a1
ryutomukai <ryuto.mukai1994@gmail.com>
2022-04-25 14:57:21 +0000

    投稿詳細ビュー画面作成

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -36,6 +36,7 @@ class Public::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @post_comment = PostComment.new
+    @post_post_comments = @post.post_comments.page(params[:page]).per(5)
   end
 
   def destroy
